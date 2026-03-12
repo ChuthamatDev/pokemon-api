976520c6583ace9b3524a3530c26a5235f831c30
Pharew Chuthamat <chuthamatbuaban@gmail.com>
2026-03-12 17:31:39 +0700

    feat: Add Docker Compose configuration for multi-service development setup and Swagger API documentation dependencies.

--- pokemon-api/dist/pokemon/pokemon.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { PokemonService, PokemonResponse, PokemonAbilityResponse } from './pokemon.service';
-export declare class PokemonController {
-    private readonly pokemonService;
-    constructor(pokemonService: PokemonService);
-    getRandomPokemon(): Promise<PokemonResponse>;
-    getPokemonByName(name: string): Promise<PokemonResponse>;
-    getPokemonAbility(name: string): Promise<PokemonAbilityResponse>;
-}
--- pokemon-api/dist/users/controllers/users.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UsersService } from '../services/users.service';
-export declare class UsersController {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    register(username: string, password: string): Promise<import("../services/users.service").RegisterResponse>;
-    login(username: string, password: string): Promise<{
-        access_token: string;
-    }>;
-}
